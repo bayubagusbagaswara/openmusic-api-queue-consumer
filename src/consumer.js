64647a543ab30c6e8da7993f3f2cb768e2218b56
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-28 19:01:17 +0700

    membuat consumer.js

---
+++ src/consumer.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const amqp = require('amqplib');
+const PlaylistsService = require('./PlaylistsService');
+const MailSender = require('./MailSender');
+const Listener = require('./listener');
+
+const init = async () => {
+  // instance dari PlaylistsService, MailSender, dan Listener
+  const playlistsService = new PlaylistsService();
+  const mailSender = new MailSender();
+  const listener = new Listener(playlistsService, mailSender);
+
+  // buat koneksi dengan server RabbitMQ
+  const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+
+  // buat channel
+  const channel = await connection.createChannel();
+
+  // pastikan queue dengan nama export:songs telah terbuat
+  await channel.assertQueue('export:songs', {
+    durable: true,
+  });
+
+  // consume queue export:songs dengan menetapkan listener.listen sebagai fungsi callback-nya
+  channel.consume('export:songs', listener.listen, { noAck: true });
+};
+
+init();
